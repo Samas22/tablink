8831334dbd9d8c910491f520afafb9c8c9bef6ae
Samael Mendes Leite <samael_leite@yahoo.com.br>
2024-12-07 19:33:06 -0300

    Teste

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+module.exports = {
+    webpack: (config, { dev }) => {
+      if (dev) {
+        config.watchOptions = {
+          ignored: [
+            '**/.dbus/**',
+            '**/.gvfs/**'
+          ]
+        };
+      }
+      return config;
+    }
+  };
+  
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1> Hello! I'm Back Again! 
+        The TRee >>>
+    </h1>
+}
+
+export default Home;
+ 
